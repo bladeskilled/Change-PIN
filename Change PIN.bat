78e26f94508724c0cbd4540fdd6ab204444311d4
BladeSkilled <109127903+bladeskilled@users.noreply.github.com>
2022-09-05 01:34:20 -0700

    Add files via upload

---
+++ Change PIN.bat
@@ -0,0 +1,40 @@
+@echo off
+CLS
+ECHO.
+ECHO Running Admin shell
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.-
+ECHO Invoking UAC for Privilege Escalation
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+cd C:\Windows\ServiceProfiles\LocalService\AppData\Local\Microsoft (the C has to be your drive letter)
+ren Ngc Old_Ngc
+@RD /S /Q "C:\Windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Old_Ngc" (the C has to be your drive letter)
+md C:\Windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc (the C has to be your drive letter)
+shutdown.exe /r /t 05
